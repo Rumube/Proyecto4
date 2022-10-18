08f0da8be3c9ae7cce2e8f3679712221285eaf31
carlacoronado207 <80068306+carlacoronado207@users.noreply.github.com>
2022-10-18 12:46:14 +0200

    Update

--- Proyect/PrototipoHabitacion/Assets/Scripts/Enemy.cs
+++ Proyect/PrototipoHabitacion/Assets/Scripts/Enemy.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public float health = 30;
+    float maxLife;
+    public Image healthBar;
     void Start()
     {
-
+        maxLife = health;
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar.fillAmount = health / maxLife;
         if (health<=0)
         {
             Destroy(gameObject);
