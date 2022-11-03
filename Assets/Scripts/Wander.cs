9610f37cc0f191761353659edc2a6fc52fe300a5
Aimofe <78077349+Aimofe@users.noreply.github.com>
2022-11-03 10:16:27 +0100

    Collision

--- Assets/Scripts/Wander.cs
+++ Assets/Scripts/Wander.cs
@@ -8,6 +8,7 @@ public class Wander : MonoBehaviour
     public float _speedRotation;
     Vector3 _randomVector;
     bool _spin=true;
+    bool _wander=true;
 
 
     public int _rotationX;
@@ -22,23 +23,57 @@ public class Wander : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += (transform.forward * _speedMove * Time.deltaTime);
-        transform.Rotate(_randomVector * _speedRotation * Time.deltaTime);
-        if (_spin==true)
+        if (_wander==true)
         {
-            _randomVector = new Vector3(Random.Range(-_rotationX, _rotationX + 1), Random.Range(-_rotationY, _rotationY+1), Random.Range(-_rotationZ, _rotationZ + 1));
-           
-            StartCoroutine("SpinTime");
-            _spin = false;
+            transform.Rotate(_randomVector * _speedRotation * Time.deltaTime);
+            transform.position += (transform.forward * _speedMove * Time.deltaTime);
+
+            transform.Rotate(_randomVector * _speedRotation * Time.deltaTime);
+            if (_spin == true)
+            {
+                _randomVector = new Vector3(Random.Range(-_rotationX, _rotationX + 1), Random.Range(-_rotationY, _rotationY + 1), Random.Range(-_rotationZ, _rotationZ + 1));
+
+                StartCoroutine("SpinTime");
+                _spin = false;
 
 
+            }
         }
+        else
+        {
+           
+            if (_randomVector==Vector3.zero)
+            {
+                transform.position += (transform.forward * -_speedMove * Time.deltaTime);
+            }
+            else
+            {
+                transform.Rotate(-_randomVector * (_speedRotation + 100) * Time.deltaTime);
+                transform.position += (transform.forward * _speedMove * Time.deltaTime);
+            }
+            
+        }
+        
       
+    }
+    void Spin()
+    {
+
     }
     IEnumerator SpinTime()
     { 
         yield return new WaitForSeconds(2);
         _spin = true;
 
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        _wander = false;
+        
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        _wander = true;
     }
 }
