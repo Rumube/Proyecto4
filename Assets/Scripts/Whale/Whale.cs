abcc26675baa7aa7cace5bf9e0af0cf4e94575f3
barreirodev <crbaro01@floridauniversitaria.es>
2022-11-29 12:40:23 +0100

    uniddo

--- Assets/Scripts/Whale/Whale.cs
+++ Assets/Scripts/Whale/Whale.cs
@@ -14,24 +14,88 @@ public class Whale : MonoBehaviour
     #region References
     [Header("References")]
     public GameObject _memoryParticle;
-    private Controller _controller;
     public Compass _compass;
+    public Material _highStar;
+    public Material _lowStar;
+    public List<GameObject> _lightsList = new List<GameObject>();
+    private PlayerController _controller;
     #endregion
+
 
     // Start is called before the first frame update
     void Start()
     {
-        _controller = GetComponent<Controller>();
+        _controller = GetComponent<PlayerController>();
+        RestartLife();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            LightUp();
+        }
+        else if (Input.GetKeyUp(KeyCode.K))
+        {
+            LightDown();
+        }
     }
+
+    public void RestartLife()
+    {
+        _currentLifePoints = _maxLifePoints;
+        UpdateLights();
+    }
+
+    public void LightDown()
+    {
+        _currentLifePoints--;
+        if (_currentLifePoints <= 0)
+        {
+            _currentLifePoints = 0;
+            print("Has perdido");
+        }
+        UpdateLights();
+    }
+
+    public void LightUp()
+    {
+        _currentLifePoints++;
+        if (_currentLifePoints >= 6)
+        {
+            _currentLifePoints = _maxLifePoints;
+        }
+        UpdateLights();
+    }
+
+    private void UpdateLights()
+    {
+        foreach (GameObject light in _lightsList)
+        {
+            light.GetComponent<Renderer>().material = _lowStar;
+        }
+
+        float newTurnSpeed = 0f;
+        float newBoostSpeed = 0f;
+
+        for (int i = 0; i < _currentLifePoints; i++)
+        {
+            _lightsList[i].GetComponent<Renderer>().material = _highStar;
+            newTurnSpeed += 20f;
+            newBoostSpeed += 4f;
+        }
+
+        _controller.SetTurnSpeed(newTurnSpeed);
+        _controller.SetboostSpeed(newBoostSpeed);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-
+        if (other.tag == "SpaceObject")
+        {
+            LightUp();
+        }
     }
 
     public Compass GetCompass()
