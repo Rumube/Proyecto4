5a8ca47993bd8ca50bec181a9e453191860c01bf
Rumube <rumube@floridauniversitaria.es>
2022-12-27 11:50:12 +0100

    PathCreator added

---
+++ Assets/Scripts/Paths/WhalePahtController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class WhalePahtController : MonoBehaviour
+{
+    public PathCreator _pathcreator;
+    public float _speed = 150;
+    float _distanceTravelled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distanceTravelled += _speed * Time.deltaTime;
+        transform.position = _pathcreator.path.GetPointAtDistance(_distanceTravelled);
+    }
+}
